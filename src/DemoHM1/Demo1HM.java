40c4ac3226664d2cab07c6c65e81bed9719f1262
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-30 10:04:37 -0400

    add

--- src/DemoHM1/Demo1HM.java
+++ src/DemoHM1/Demo1HM.java
@@ -3,6 +3,11 @@ package DemoHM1;
 public class Demo1HM {
 	public static void main(String[] args) {
 		System.out.println("Yasir");
+		System.out.println("Aldarraji");
+	}
+		public static void add (int a , int b)	{
+			System.out.println(a + b);
+		}
 	}
 
-}
+
